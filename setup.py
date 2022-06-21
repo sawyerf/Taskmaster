286defee7315b470768552160b232539e0d65f10
Sawyerf <sawyer.flink@protonmail.ch>
2022-06-21 12:05:31 +0200

    Add: setup.py && venv

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools	import setup, find_packages
+
+setup(
+	name='Taskmaster',
+	description='This program is a job control task.',
+	long_description_content_type='text/markdown',
+	packages=find_packages(include=[
+		'taskmaster', 'taskmaster.*',
+	]),
+	entry_points={
+		'console_scripts': [
+			'taskmasterd = taskmaster.taskmasterd:main',
+			'taskmasterctl = taskmaster.taskmasterctl:main'
+		]
+	}
+)
